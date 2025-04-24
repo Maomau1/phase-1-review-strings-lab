535d0df41b5bc84259174b4131d3385871cc245c
maurice diong <mdiong00@gmail.com>
2025-04-24 17:30:58 -0400

    completed lab 2. also intro to slice method

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper';
+// const currentUser = 'Grace Hopper';
 
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = ` Welcome, ${ currentUser.slice(0,1)}!`;
+
+let currentUser = 'Grace Hopper';
+
+const welcomeMessage = ` Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = ` Welcome, ${ currentUser.slice(0,1)}!`;
+const shortGreeting = `Welcome, ${currentUser[0].slice(0,1)}!`;
